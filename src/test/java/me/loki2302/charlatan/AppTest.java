ba2bcdc6c61f9f7e5b6d7062d1910e9b21fb6c56
loki2302 <loki2302@loki2302.me>
2014-03-20 23:05:13 +0400

    Added random option generator test

--- src/test/java/me/loki2302/charlatan/AppTest.java
+++ src/test/java/me/loki2302/charlatan/AppTest.java
@@ -2,6 +2,8 @@ package me.loki2302.charlatan;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -11,5 +13,24 @@ public class AppTest {
         String firstName = charlatan.firstName();
         assertNotNull(firstName);
         assertFalse(firstName.isEmpty());
-    }    
+    }
+    
+    @Test
+    public void randomOptionGeneratorWorks() {
+        RandomOptionGenerator<Integer> rog = new RandomOptionGenerator<Integer>()
+                .withOption(0, 1)
+                .withOption(1, 10)
+                .withOption(2, 100);
+        
+        int[] counts = new int[3];
+        Arrays.fill(counts, 0);
+        
+        for(int i = 0; i < 10000; ++i) {
+            int sample = rog.generate();
+            ++counts[sample];
+        }
+        
+        assertTrue(counts[0] <= counts[1]);
+        assertTrue(counts[1] <= counts[2]);        
+    }
 }
